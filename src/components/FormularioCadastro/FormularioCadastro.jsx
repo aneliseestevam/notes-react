daacf99a832ee5c1d0ed2c1dd7facff6bb780cd7
aneliseestevam <anelisee1@gmail.com>
2021-08-28 21:43:08 -0300

    Updating the react app structure and adding title change manipulation function

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class FormularioCadastro extends Component {
+
+	constructor() {
+		super();
+		this.titulo = "";
+	}
+
+	handleChangeTitle(event) {
+		this.titulo = event.target.value;
+		console.log(this.titulo);
+	}
+
+	render() {
+		return (
+			<form className="form-cadastro ">
+				<input
+					type="text"
+					placeholder="Título"
+					className="form-cadastro_input"
+					onChange={this.handleChangeTitle.bind(this)}
+				/>
+				<textarea
+					rows={15}
+					placeholder="Escreva sua nota..."
+					className="form-cadastro_input"
+				/>
+				<button className="form-cadastro_input form-cadastro_submit">
+					Criar Nota
+				</button>
+			</form>
+		);
+	}
+}
+
+export default FormularioCadastro;
